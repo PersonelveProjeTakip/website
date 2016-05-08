3ceac3ba1d051d3157524009c9f6be44580fb194
Cemal <cemaltaskiran@gmail.com>
2016-05-08 19:54:30 +0300

    Final Version

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <img src="images/icon_chrome.gif" title="Chrome"><p class="left">Bu site en iyi Google Chrome ile görüntülenir.</p>
+    <p class="right">© 2016 Copyright Nebula Software Development</p>
+</footer>
--- index.php
+++ index.php
@@ -114,7 +114,9 @@
                     </div>
                 </div>
         </article>
+        <?php include('footer.php'); ?>
     </main>
+    
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>  
